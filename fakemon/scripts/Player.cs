6597f273f6df6b6fede55af3e08c83ac4076c90c
Lennart-schlass <lennart.schlass@stud.hn.de>
2025-12-07 19:03:56 +0100

    Add files via upload

--- fakemon/scripts/Player.cs
+++ fakemon/scripts/Player.cs
@@ -1,20 +1,22 @@
 using Godot;
 using System;
 
-public partial class Player : Node2D
+public partial class Player : AnimatedSprite2D 
 {
-	[Export] public float MoveSpeed = 50f;
+	[Export] public float MoveSpeed = 100f;
 	[Export] public int GridSize = 32;
 
 	private Vector2 targetPosition;
 	private bool isMoving = false;
-
-	private AnimatedSprite2D anim;
+	private int last = -1;
+	private double pressTime = 0.0;
+	private const double tapThreshold = 0.050;
+	private bool directionPressed = false;
+	private Vector2 lastInputDir = Vector2.Zero;
+	private Vector2 heldDir = Vector2.Zero;
 
 	public override void _Ready()
-	{
-		anim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-
+	{		
 		targetPosition = GlobalPosition;
 	}
 
@@ -30,28 +32,35 @@ public partial class Player : Node2D
 	{
 		Vector2 input = Vector2.Zero;
 
-		if (Input.IsActionPressed("ui_up"))
+		if (Input.IsActionPressed("ui_up"))      input = Vector2.Up;
+		else if (Input.IsActionPressed("ui_down"))  input = Vector2.Down;
+		else if (Input.IsActionPressed("ui_left"))  input = Vector2.Left;
+		else if (Input.IsActionPressed("ui_right")) input = Vector2.Right;
+
+		if (input == Vector2.Zero)
 		{
-			input = Vector2.Up;
-			PlayAnimation("up");
-		}
-		else if (Input.IsActionPressed("ui_down"))
-		{
-			input = Vector2.Down;
-			PlayAnimation("down");
-		}
-		else if (Input.IsActionPressed("ui_left"))
-		{
-			input = Vector2.Left;
-			PlayAnimation("left");
-		}
-		else if (Input.IsActionPressed("ui_right"))
-		{
-			input = Vector2.Right;
-			PlayAnimation("right");
+			directionPressed = false;
+			pressTime = 0;
+			if (!isMoving) Stop();
+			return;
 		}
 
-		if (input != Vector2.Zero)
+		if (!directionPressed || input != heldDir)
+		{
+			directionPressed = true;
+			heldDir = input;
+			pressTime = 0;
+
+			if (input == Vector2.Up)    PlayAnimation("up");
+			if (input == Vector2.Down)  PlayAnimation("down");
+			if (input == Vector2.Left)  PlayAnimation("left");
+			if (input == Vector2.Right) PlayAnimation("right");
+
+			return; 
+
+		pressTime += GetProcessDeltaTime();
+
+		if (pressTime >= tapThreshold && !isMoving)
 		{
 			targetPosition += input * GridSize;
 			isMoving = true;
@@ -60,21 +69,27 @@ public partial class Player : Node2D
 
 	private void MoveTowardsTarget(double delta)
 	{
-		if (!isMoving)
-			return;
+		if (!isMoving) return;
 
 		GlobalPosition = GlobalPosition.MoveToward(targetPosition, MoveSpeed * (float)delta);
 
 		if (GlobalPosition == targetPosition)
 		{
 			isMoving = false;
-			anim.Stop();  
+
+			if (!Input.IsActionPressed("ui_up") &&
+				!Input.IsActionPressed("ui_down") &&
+				!Input.IsActionPressed("ui_left") &&
+				!Input.IsActionPressed("ui_right"))
+			{
+				Pause();
+			}
 		}
 	}
 
 	private void PlayAnimation(string animName)
 	{
-		if (!anim.IsPlaying() || anim.Animation != animName)
-			anim.Play(animName);
+		if (!IsPlaying() || Animation != animName)
+			Play(animName);
 	}
 }
